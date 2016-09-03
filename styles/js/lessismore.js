3ee4f7b4847622ce9eee66bbfa667761cf589ffe
luoyan35714 <luoyan35714@126.com>
2016-09-03 22:13:38 +0800

    change the pictures to normal size not max than 757

--- styles/js/lessismore.js
+++ styles/js/lessismore.js
@@ -6,9 +6,28 @@
 !function ($) {
 
   $(function () {
-      $('#navigation .sidenav').html($("#markdown-toc").html());
-      $('#navigation .sidenav ul').addClass("nav");
-      $("#markdown-toc").remove();
+      
+		orderTheLeftNavigations();
+
+      	function orderTheLeftNavigations(){
+		    $('#navigation .sidenav').html($("#markdown-toc").html());
+		    $('#navigation .sidenav ul').addClass("nav");
+		    $("#markdown-toc").remove();
+      	}
+
+      	$(window).load(repairTheImagesWhichCrossTheMaxWidth);
+
+      	function repairTheImagesWhichCrossTheMaxWidth(){
+      		var images = $(".docs-content img");
+      		if(images != undefined && images.length > 0){
+      			for(var i=0; i< images.length;i++){
+      				var imgWidth = images[i].width;
+      				if( imgWidth >= 757 ){
+      					 images[i].width = 757;
+      				}
+      			}
+      		}
+      	}
   })
 
 }(jQuery)
